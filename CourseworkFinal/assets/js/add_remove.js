900eba4e44e2ec204e269b425b59072fd7105783
Divs1 <divyakbedi@yahoo.co.uk>
2016-02-15 16:04:11 +0000

    Initial upload of Project

---
+++ CourseworkFinal/assets/js/add_remove.js
@@ -0,0 +1,91 @@
+        $(document).ready(function(){
+ $("#hide").click(function(){
+    $("div.textfave").hide();
+  });
+  $("#show").click(function(){
+    $("div.textfave").show();
+  });
+});
+
+        $(document).ready(function(){
+ $("#hide2").click(function(){
+    $("div.textfave2").hide();
+  });
+  $("#show2").click(function(){
+    $("div.textfave2").show();
+  });
+});
+
+        $(document).ready(function(){
+ $("#hide3").click(function(){
+    $("div.textfave3").hide();
+  });
+  $("#show3").click(function(){
+    $("div.textfave3").show();
+  });
+});
+
+        $(document).ready(function(){
+ $("#hide4").click(function(){
+    $("div.textfave4").hide();
+  });
+  $("#show4").click(function(){
+    $("div.textfave4").show();
+  });
+});
+
+        $(document).ready(function(){
+ $("#hide5").click(function(){
+    $("div.textfave5").hide();
+  });
+  $("#show5").click(function(){
+    $("div.textfave5").show();
+  });
+});
+
+        $(document).ready(function(){
+ $("#hide6").click(function(){
+    $("div.textfave6").hide();
+  });
+  $("#show6").click(function(){
+    $("div.textfave6").show();
+  });
+});
+
+        $(document).ready(function(){
+ $("#hide7").click(function(){
+    $("div.textfave7").hide();
+  });
+  $("#show7").click(function(){
+    $("div.textfave7").show();
+  });
+});
+
+        $(document).ready(function(){
+ $("#hide8").click(function(){
+    $("div.textfave8").hide();
+  });
+  $("#show8").click(function(){
+    $("div.textfave8").show();
+  });
+});
+
+        $(document).ready(function(){
+ $("#clearall").click(function(){
+    $("div.textfave").hide(),
+    $("div.textfave2").hide(),
+    $("div.textfave3").hide(),
+    $("div.textfave4").hide(),
+    $("div.textfave5").hide(),
+    $("div.textfave6").hide(),
+    $("div.textfave7").hide(),
+    $("div.textfave8").hide();
+  });
+});
+
+
+
+
+
+
+
---
+++ CourseworkFinal/assets/js/default.js
@@ -0,0 +1,44 @@
+/*
+ *
+ *  Search Bar
+ *
+ */
+
+$(document).ready( function() {
+
+    $('.search a').on('click', function() {
+
+        $('.search-bar').slideDown('fast'); // display search bar
+
+        $('.close').click( function() { // close search bar
+            $('.search-bar').slideUp('fast');
+            $('.search-bar').removeClass('hide'); // remove class hide on hide
+        });
+
+        return false;
+
+    });
+
+    $('aside .member').each( function(i) { // Team hover up and down
+
+        var id      = ('ui-' + i); // generate unique ID
+        $(this).addClass(id); // add class
+        var info    = $('.'+id+' .info-further'); // info unique identifier
+
+        $(this).hover( function() {
+
+            info.slideDown('fast');
+            $('.'+id+' .indicator').removeClass('fa-angle-down');
+            $('.'+id+' .indicator').addClass('fa-angle-up');
+
+        }, function() {
+
+            info.slideUp('fast');
+            $('.'+id+' .indicator').removeClass('fa-angle-up');
+            $('.'+id+' .indicator').addClass('fa-angle-down');
+
+        });
+
+    });
+
+});
